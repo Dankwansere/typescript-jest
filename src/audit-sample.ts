83ca50c9e3cdbd465920111715cc6e8ff821fd4e
Eric <ericsarpong@hotmail.com>
2020-04-30 23:05:20 -0400

    updated

--- src/audit-sample.ts
+++ src/audit-sample.ts
@@ -1,14 +1,16 @@
-import {of, Observable, timer} from 'rxjs'
-import {audit} from 'rxjs/operators';
+import {of, Observable, timer, interval} from 'rxjs'
+import {audit, take} from 'rxjs/operators';
 export class AuditTest {
     constructor() {}
 
-    public testAudit(arr: Array<number>): Observable<any> {
-        return of(...arr).pipe(
+    public testAudit(): Observable<any> {
+        const source = interval(100);
+        return source.pipe(
+            take(20),
             audit(x => {
                 console.log('aduit operator received value: ', x);
-                return this.delayedMethod();
-                //return timer(50);
+                
+            return timer(500);
             })
         )
     }
@@ -26,3 +28,7 @@ export class AuditTest {
         return 'hello world'
     }
 }
+const audit2 = new AuditTest();
+audit2.testAudit().subscribe(x => {
+    console.log('value from subscribe source: ', x);
+})
